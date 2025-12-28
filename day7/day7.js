41d02783c4fd9c7935abdeaa4b7dc4b7c323e12c
Noah van Boven <noah.van.boven@student.uva.nl>
2025-12-28 13:36:21 +0100

    Optimizing day 7

--- day7/day7.js
+++ day7/day7.js
@@ -2,24 +2,10 @@ import { readFileSync } from 'fs';
 
 const input = readFileSync('input.txt').toString().trim().split("\n")
 
-const increaseCoord = (coord, dx, dy) => {
-    const [x, y] = coord.split(',').map(Number);
-    return [x + dx, y + dy].join(',');
-}
-
 const part1 = (input) => {
     // I don't know if making a Set is more efficient vs an array with elements
     // {x, y} and using Array#search() each time instead of splitters.includes.
-    let beams = new Set([`${input[0].indexOf('S')},0`]);
-
-    let splitters = []
-    for (const [i, str] of input.entries()) {
-        // Match all the ^ in the current row, and map it to coordinates using
-        // the index parameter in each match.
-        const rowSplitters = [...str.matchAll(/(\^)/g)].map(e => (`${e['index']},${i}`))
-
-        splitters = [...splitters, ...rowSplitters]
-    }
+    let beams = new Set([input[0].indexOf('S')]);
 
     let split = 0;
     let y = 1;
@@ -27,14 +13,10 @@ const part1 = (input) => {
         // If it hits a splitter, you split it.
         const currBeams = [...beams]
         for (const beam of currBeams) {
-            let newBeam = increaseCoord(beam, 0, 1);
-            beams.delete(beam);
-            beams.add(newBeam);
-
-            if (splitters.includes(newBeam)) {
-                beams.delete(newBeam);
-                beams.add(increaseCoord(newBeam, -1, 0));
-                beams.add(increaseCoord(newBeam, 1, 0));
+            if (input[y][beam] === '^') {
+                beams.delete(beam);
+                beams.add(beam - 1);
+                beams.add(beam + 1);
 
                 split++;                
             }
@@ -50,36 +32,21 @@ const part2 = (input) => {
     // We make the Set a Map so we can add weights so we know much much beams
     // are in this beam so to say.
     let beams = new Map();
-    beams.set(`${input[0].indexOf('S')},0`, 1);
-
-    let splitters = []
-    for (const [i, str] of input.entries()) {
-        // Match all the ^ in the current row, and map it to coordinates using
-        // the index parameter in each match.
-        const rowSplitters = [...str.matchAll(/(\^)/g)].map(e => (`${e['index']},${i}`))
-
-        splitters = [...splitters, ...rowSplitters]
-    }
+    beams.set(input[0].indexOf('S'), 1);
 
     let y = 1;
     while (y < input.length) {
-        // Change the map so moves every beam down by 1. We need to do this
-        // before the loop because otherwise you can miss a "double split" where
-        // you check for a beam that should be there but hasn't been moved
-        // down by 1 yet.
-        let currBeams = [...beams]
-            .map(([beam, weight]) => [increaseCoord(beam, 0, 1), weight]);
-        // Update the beams.
-        beams = new Map(currBeams)
+        const currBeams = [...beams]
 
         // Loop over the temporary currBeams, so you don't endlessly loop over 
         // the beams map which you update in the loop.
         for (const [newBeam, weight] of currBeams) {
+
             // If we hit a splitter we split it, update the weights.
-            if (splitters.includes(newBeam)) {
+            if (input[y][newBeam] === '^') {
                 beams.delete(newBeam);
-                const left = increaseCoord(newBeam, -1, 0);
-                const right = increaseCoord(newBeam, 1, 0);
+                const left = newBeam - 1;
+                const right = newBeam + 1;
                 beams.set(left, beams.has(left) ? beams.get(left) + weight : weight)
                 beams.set(right, beams.has(right) ? beams.get(right) + weight : weight)
             }
